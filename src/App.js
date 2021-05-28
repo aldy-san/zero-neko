7d2e7fa77cba6ba6251724c4f89a1773e2942cdc
aldy-san <nsmaulidi12@gmail.com>
2021-05-28 13:37:28 +0700

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-blue-600">
+          Edit <code>src/App.js</code> aasand save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-red-700"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
