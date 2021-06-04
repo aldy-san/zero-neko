1def7ecd9223aa000606a7838b1bade5c4822d0c
aldy-san <nsmaulidi12@gmail.com>
2021-06-04 19:15:48 +0700

    update Footer

--- src/components/ChildFooter.js
+++ src/components/ChildFooter.js
@@ -3,11 +3,11 @@ import React from 'react'
 const ChildFooter = (props) =>{
     return(
         <div className="col-3">
-            <h3 className="font-semibold text-medium tracking-widest">{props.title}</h3>
-            <ul className="space-y-2 mt-3">
+            <h3 className="font-semibold text-sm lg:text-base text-medium tracking-widest">{props.title}</h3>
+            <ul className="space-y-1 lg:space-y-2 mt-2 lg:mt-3">
                 {
                     props.data.map((nav, index) => {
-                        return <li className="transition-all delay-75 hover:text-primary" key={index}><a href={nav.path} >{nav.title}</a></li>
+                        return <li className="transition-all delay-75 text-sm lg:text-base hover:text-primary" key={index}><a href={nav.path} >{nav.title}</a></li>
                     })
                 }
             </ul>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,11 +16,11 @@ const Footer = () => {
     return(
         <div className="bg-gray-100 dark:bg-gray-700 dark:text-white py-8">
             <div className="grid grid-cols-12 mx-8 lg:mx-32">
-                <div className="col-span-12 lg:col-span-3 mb-8 lg:mb-0">
-                    <img className="mx-auto mb-5" src={(theme !== "dark" ? logoIcon : logoIconWhite)} alt="logo-full" width="100" />
-                    <p className="text-center lg:text-left w-52 mx-auto">Zeroneko is website platform for everyone to learn Japanese.</p>
+                <div className="col-span-12 lg:col-span-3 mb-4 lg:mb-8 lg:mb-0">
+                    <img className="mx-auto mb-5 w-20 lg:w-24" src={(theme !== "dark" ? logoIcon : logoIconWhite)} alt="logo-full" />
+                    <p className="text-center text-base lg:text-medium lg:text-left w-60 lg:w-52 mx-auto">Zeroneko is website platform for everyone to learn Japanese.</p>
                 </div>
-                <div className="grid grid-cols-1 lg:grid-cols-3 col-span-12 lg:col-span-9 space-y-6 lg:space-y-0 lg:ml-20 text-center lg:text-left">
+                <div className="grid grid-cols-3 lg:grid-cols-3 col-span-12 lg:col-span-9 space-y-0 mt-5 lg:mt-0 lg:ml-20 text-center lg:text-left">
                     <ChildFooter title={'Navigation'} data={navLinks}/>
                     <ChildFooter title={'Sosial Media'} data={socialMedia}/>
                     <ChildFooter title={'About Developer'} data={me}/>
