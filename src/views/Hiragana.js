82cdf2e15e81848f7cb7cf3116fd1298c4807015
aldy-san <nsmaulidi12@gmail.com>
2021-06-05 15:56:26 +0700

    Bug Fix Hiragana

--- src/views/Hiragana.js
+++ src/views/Hiragana.js
@@ -5,7 +5,7 @@ const Hiragana = () => {
     return(
         <>
             <div className="text-center lg:mx-36">
-                <h1 className="text-6xl mt-8 mb-12 text-left"><span className="font-black rounded-full">あ</span> Hiragana</h1>
+                <h1 className="text-6xl mt-8 mb-12 text-center lg:text-left"><span className="font-black rounded-full block lg:inline">あ</span> Hiragana</h1>
                 <div className="grid grid-cols-1 lg:grid-cols-2 px-3 py-4 lg:p-6 shadow-inner mx-1 bg-gray-200 dark:bg-gray-600 rounded-lg space-y-4 lg:space-y-0">
                     <ul className="grid grid-cols-5 auto-rows-min grid-span-1 gap-2 lg:gap-4 lg:p-4">
                         {
@@ -14,7 +14,7 @@ const Hiragana = () => {
                                     return true;
                                 }
                                 return(
-                                    <li className={"box-border transition-all delay-75 grid-span-1 col-start-"+hira.start+" rounded-md bg-gray-50 dark:bg-gray-700 box-content p-4 shadow-md dark:shadow-mdWhite dark:hover:shadow-none hover:shadow-none"} key={index}>
+                                    <li className={"box-border transition-all delay-75 grid-span-1"+(hira.start ? " col-start-"+hira.start : "")+" rounded-md bg-gray-50 dark:bg-gray-700 box-content p-4 shadow-md dark:shadow-mdWhite dark:hover:shadow-none hover:shadow-none"} key={index}>
                                         <p className="text-xl lg:text-3xl font-black mb-2">{hira.kana}</p>
                                         <p className="font-semibold text-sm lg:text-base text-primary">{hira.romaji}</p>
                                     </li>
@@ -30,7 +30,7 @@ const Hiragana = () => {
                                         return true;
                                     }
                                     return(
-                                        <li className={"transition-all delay-75 col-span-1 col-start-"+hira.start+" rounded-md bg-gray-50 dark:bg-gray-700 box-content p-4 shadow-md dark:shadow-mdWhite dark:hover:shadow-none hover:shadow-none"} key={index}>
+                                        <li className={"transition-all delay-75 col-span-1 "+(hira.start ? "col-start-"+hira.start : "")+" rounded-md bg-gray-50 dark:bg-gray-700 box-content p-4 shadow-md dark:shadow-mdWhite dark:hover:shadow-none hover:shadow-none"} key={index}>
                                             <p className="text-xl lg:text-3xl font-black mb-2">{hira.kana}</p>
                                             <p className="font-semibold text-sm lg:text-base text-primary">{hira.romaji}</p>
                                         </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'mdWhite': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.06)',
+        'mdWhite': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.03)',
       },
       colors: {
         primary: "#f86d70"
