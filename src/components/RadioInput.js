f38134e02e05916bb4336773c890f0edb278a29f
aldy-san <nsmaulidi12@gmail.com>
2021-06-20 19:47:03 +0700

    Add Kanji Search Filter

--- src/components/RadioInput.js
+++ src/components/RadioInput.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const RadioInput = (props) =>{
+    // console.log(props.checked.split("/")[1]);
+    // console.log("grade-"+props.grade);
+    console.log();
     return(
-        <label className="bg-gray-600 rounded-lg text-base p-2 cursor-pointer" htmlFor={"grade-"+props.grade}>
-            <input className="mr-2" type="radio" id={"grade-"+props.grade} name="grade" value={"grade-"+props.grade} onClick={() => props.onSendGrade("grade-"+props.grade)}/>
+        <label className={(props.checked.split("/")[1] === "grade-"+props.grade ? "bg-gray-200 dark:bg-gray-900 shadow-inner" : "hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 hover:bg-opacity-90 shadow")+" rounded-lg text-base p-2 cursor-pointer"} htmlFor={"grade-"+props.grade}>
+            <input className="mr-2 hidden" type="radio" id={"grade-"+props.grade} name="grade" value={"grade-"+props.grade} onClick={() => props.onSendGrade("grade-"+props.grade)}/>
             Grade-{props.grade}
         </label>
     )
--- src/views/Kanji.js
+++ src/views/Kanji.js
@@ -56,6 +56,7 @@ const Kanji = () => {
             console.log(newFilter);
             setFilter("reading/"+newFilter)
         }
+        setModal(false);
     }
     // const loading = (
     //     <li >
@@ -69,46 +70,49 @@ const Kanji = () => {
     return(
         <div className="relative text-center lg:mx-36">
             <H1 span={"漢字"} text={"Kanji"}/>
-            <div className="flex py-4 px-1 ">
-                <div className="flex flex-col">
-                    <span className="text-sm text-white pb-3 pl-1 pt-0 text-left">{'Searched "'+filter.split("/")[1]+'"'}</span>
-                    <div className="flex rounded-lg dark:bg-gray-700 shadow">
-                        <div className="p-3 rounded-l-lg bg-gray-100 border-r-2 border-gray-300 dark:bg-gray-600 dark:border-gray-500">
-                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"/>
-                            </svg>
+            <div className="flex flex-col py-4 px-1 ">
+                <span className="text-sm pb-3 pl-1 pt-0 text-left">{'Filter: Searched "'+filter.split("/")[1]+'"'}</span>
+                <div className="flex border-t-2 pt-4 dark:border-gray-700">
+                    <div className="flex">
+                        <div className="flex rounded-lg dark:bg-gray-700 shadow">
+                            <div className="p-3 rounded-l-lg bg-gray-100 border-r-2 border-gray-300 dark:bg-gray-600 dark:border-gray-500">
+                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"/>
+                                </svg>
+                            </div>
+                            <input className="bg-transparent outline-none py-3 px-6" type="search" name="searchKanji" id="searchKanji" onChange={(e) => changeFilter(e.target.value)} placeholder="Search" />
                         </div>
-                        <input className="bg-transparent outline-none py-3 px-6" type="search" name="searchKanji" id="searchKanji" onChange={(e) => changeFilter(e.target.value)} placeholder="Search" />
+                    </div>
+                    {/* Modal Button */}
+                    <div className="relative ml-2 my-auto">
+                        <button onClick={() => {modal === true ? setModal(false) : setModal(true)}} className="flex bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-xl shadow hover:bg-gray-100 text-lg px-4 py-2 cursor-pointer">
+                            <span>Grade</span>
+                            <svg className="transform scale-50 ml-2 my-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                                <path stroke="currentColor" d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
+                            </svg>
+                        </button>
+                        {/* Modal */}
+                        <div className={(modal ? "flex" : "hidden")+" absolute top-14 rounded-lg w-40 bg-white shadow-lg dark:bg-gray-700 flex-col ml-auto px-2 py-3"}>
+                            {/* <button className="absolute right-2 top-2 bg-blue-600" onClick={() => setModal(false)}>
+                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" d="M17.25 6.75L6.75 17.25"></path>
+                                    <path stroke="currentColor" d="M6.75 6.75L17.25 17.25"></path>
+                                </svg>
+                            </button> */}
+                            <div className="flex flex-col space-y-2">
+                                <RadioInput grade="1" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="2" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="3" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="4" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="5" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="6" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                                <RadioInput grade="8" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
+                            </div>
+                        </div>
+                        {/* Modal */}
                     </div>
                 </div>
-                {/* Modal Button */}
-                <div className="ml-auto mt-auto">
-                    <button onClick={() => {setModal(true)}} className="flex bg-gray-50 dark:bg-gray-600 dark:hover:bg-gray-700 rounded-lg shadow hover:bg-gray-100 text-lg py-3 px-6 cursor-pointer">
-                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" d="M19.25 4.75H4.75L9.31174 10.4522C9.59544 10.8068 9.75 11.2474 9.75 11.7016V18.25C9.75 18.8023 10.1977 19.25 10.75 19.25H13.25C13.8023 19.25 14.25 18.8023 14.25 18.25V11.7016C14.25 11.2474 14.4046 10.8068 14.6883 10.4522L19.25 4.75Z"></path>
-                        </svg>
-                        <span className="ml-2">Filter</span>
-                    </button>
-                </div>
-                {/* Modal */}
-                <div className={(modal ? "flex" : "hidden")+" fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-96 w-72 bg-red-500 flex-col ml-auto p-8 "}>
-                    <button className="absolute right-2 top-2 bg-blue-600" onClick={() => setModal(false)}>
-                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" d="M17.25 6.75L6.75 17.25"></path>
-                            <path stroke="currentColor" d="M6.75 6.75L17.25 17.25"></path>
-                        </svg>
-                    </button>
-                    <div className="flex flex-col space-y-2">
-                        <RadioInput grade="1" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="2" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="3" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="4" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="5" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="6" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                        <RadioInput grade="8" checked={filter} onSendGrade={(theGrade) => changeFilter(theGrade)}/>
-                    </div>
-                </div>
-                {/* Modal */}
+                
             </div>
             <ul className="grid grid-cols-5 items-center lg:grid-cols-10 px-3 py-4 lg:p-6 mx-1 gap-2 lg:gap-3 rounded-lg lg:space-y-0 bg-gray-200 dark:bg-gray-900 shadow-inner">
                 {
