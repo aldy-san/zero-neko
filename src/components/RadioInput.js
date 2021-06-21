b3b660ecca71da8306383f31cf9be3cb74edb777
aldy-san <nsmaulidi12@gmail.com>
2021-06-21 19:31:11 +0700

    Grade Disabled

--- src/components/RadioInput.js
+++ src/components/RadioInput.js
@@ -6,7 +6,7 @@ const RadioInput = (props) =>{
     console.log();
     return(
         <label className={(props.checked.split("/")[1] === "grade-"+props.grade ? "bg-gray-200 dark:bg-gray-900 shadow-inner" : "hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 hover:bg-opacity-90 shadow")+" rounded-lg text-base p-2 cursor-pointer"} htmlFor={"grade-"+props.grade}>
-            <input className="mr-2 hidden" type="radio" id={"grade-"+props.grade} name="grade" value={"grade-"+props.grade} onClick={() => props.onSendGrade("grade-"+props.grade)}/>
+            <input disabled={props.checked.split("/")[1] === "grade-"+props.grade} className="mr-2 hidden" type="radio" id={"grade-"+props.grade} name="grade" value={"grade-"+props.grade} onClick={() => props.onSendGrade("grade-"+props.grade)}/>
             Grade-{props.grade}
         </label>
     )
