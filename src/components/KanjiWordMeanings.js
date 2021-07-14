9418ba88e19f59d0167f34eb3399a128b01e67cd
aldy-san <nsmaulidi12@gmail.com>
2021-07-14 17:01:24 +0700

    fix words

--- src/components/KanjiWordMeanings.js
+++ src/components/KanjiWordMeanings.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const KanjiWordMeanings = (props) =>{
     return(
-        <p className="text-xs lg:text-sm text-right">{(props.num === -1 ? "" : props.num+1+". ")+props.datas.glosses[0][0].toUpperCase()+props.datas.glosses.join("; ").slice(1)}</p>
+        <p className="text-xs lg:text-sm ">{(props.num === -1 ? "" : props.num+1+". ")+props.datas.glosses[0][0].toUpperCase()+props.datas.glosses.join("; ").slice(1)}</p>
     )
 }
 
--- src/components/KanjiWordsList.js
+++ src/components/KanjiWordsList.js
@@ -5,32 +5,30 @@ import { Link } from 'react-router-dom'
 
 const KanjiWordsList = (props) =>{
     return(
-        <div className="flex flex-col border-b border-gray-400 py-4">
-            <div className="flex">
-                <p className="whitespace-nowrap text-2xl lg:text-4xl my-auto mr-auto font-semibold">
-                    <span className="space-x-2">
-                        {
-                            props.datas.variants[0].written.split("").map((kana, idx) => {
-                                if (isKanji(kana)) {
-                                    return <Link key={idx} to={"/kanji/"+kana} className={isKanji(kana) ? "transition-all duration-150 border-dashed border-b-2 border-current hover:border-primary hover:text-5xl hover:text-primary" : ""}>{kana}</Link>;
-                                }
-                                return <span key={idx}>{kana}</span>;
-                            })
-                        }
-                    </span>
-                </p>
-                <div  className="text-right w-36 lg:w-96">
-                    <div className="mb-3">
-                        <p className="text-xs lg:text-sm text-primary mb-2">{props.datas.variants[0].pronounced ? toRomaji(props.datas.variants[0].pronounced) :""}</p>
-                        <p className="text-base lg:text-2xl font-semibold">{props.datas.variants[0].pronounced ? props.datas.variants[0].pronounced :""}</p>
-                    </div>
-                    <div className="flex flex-col">
+        <div className="flex flex-col lg:flex-row border-b border-gray-400 py-4 space-y-4 lg:space-y-0">
+            <p className="whitespace-nowrap text-2xl lg:text-4xl my-auto mr-auto font-semibold">
+                <span className="space-x-2">
                     {
-                        props.datas.meanings.map((meaning, index) => {
-                            return <KanjiWordMeanings key={index} num={props.datas.meanings.length > 1 ? index : -1} datas={meaning}/>
+                        props.datas.variants[0].written.split("").map((kana, idx) => {
+                            if (isKanji(kana)) {
+                                return <Link key={idx} to={"/kanji/"+kana} className={isKanji(kana) ? "transition-all duration-150 border-dashed border-b-2 border-current hover:border-primary hover:text-5xl hover:text-primary" : ""}>{kana}</Link>;
+                            }
+                            return <span key={idx}>{kana}</span>;
                         })
                     }
-                    </div>
+                </span>
+            </p>
+            <div  className="lg:text-right w-36 lg:w-96">
+                <div className="mb-3">
+                    <p className="text-xs lg:text-sm text-primary mb-2">{props.datas.variants[0].pronounced ? toRomaji(props.datas.variants[0].pronounced) :""}</p>
+                    <p className="text-base lg:text-2xl font-semibold">{props.datas.variants[0].pronounced ? props.datas.variants[0].pronounced :""}</p>
+                </div>
+                <div className="flex flex-col">
+                {
+                    props.datas.meanings.map((meaning, index) => {
+                        return <KanjiWordMeanings key={index} num={props.datas.meanings.length > 1 ? index : -1} datas={meaning}/>
+                    })
+                }
                 </div>
             </div>
         </div>
