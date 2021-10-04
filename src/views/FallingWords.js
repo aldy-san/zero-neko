f19c7c9009d804a76913e5ed7841fc36652471d2
Specy <specy.dev@gmail.com>
2021-10-04 10:24:21 +0200

    fixed errors, added romanization

--- src/views/FallingWords.js
+++ src/views/FallingWords.js
@@ -1,40 +1,53 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { all } from "../data/words/all";
-import { isKana, toRomaji } from 'wanakana';
+import { toRomaji,isHiragana, isKatakana } from 'wanakana';
 import { Stage, Text, Graphics } from '@inlet/react-pixi'
 import { TextStyle } from 'pixi.js';
 const FallingWords = () => {
     const [words, setWords] = useState([]);
-    const [isStarted, setIsStarted] = useState(true);
+    const [isStarted, setIsStarted] = useState(false);
     const [selectedDifficulty, setDifficulty] = useState(0);
     const [currentText, setCurrentText] = useState('');
     const [inputColor, changeInputColor] = useState('');
     const [score, setScore] = useState(0);
+    const [chosenAlphabet, setChosenAlphabet] = useState('hiragana');
     const difficulties = [
         {
             id: 0,
             name: 'Beginner',
             speed: 3,
-            words: all.filter(word => word.expression.length < 4)
+            words: {
+                hiragana: all.filter(word => word.expression.length < 4 && isHiragana(word.expression)),
+                katakana: all.filter(word => word.expression.length < 4 && isKatakana(word.expression)),
+            }
         },{
             id: 1,
             name: 'Easy',
             speed: 8,
-            words: all.filter(word => word.expression.length < 4)
+            words: {
+                hiragana: all.filter(word => word.expression.length < 4 && isHiragana(word.expression)),
+                katakana: all.filter(word => word.expression.length < 4 && isKatakana(word.expression)),
+            }
         },{
             id: 2,
             name: 'Medium',
             speed: 12,
-            words: all.filter(word => word.expression.length < 5 && word.expression.length > 3)
+            words: {
+                hiragana: all.filter(word => word.expression.length < 5 && isHiragana(word.expression) && word.expression.length > 3),
+                katakana: all.filter(word => word.expression.length < 5 && isKatakana(word.expression) && word.expression.length > 3),
+            }
         },{
             id: 3,
             name: 'Hard',
             speed: 16,
-            words: all.filter(word => word.expression.length > 3)
+            words: {
+                hiragana: all.filter(word => word.expression.length > 3 && isHiragana(word.expression)),
+                katakana: all.filter(word => word.expression.length > 3 && isKatakana(word.expression)),
+            }
         }
     ]
 
-    const [game, setGame] = useState(new Game(difficulties[selectedDifficulty]))
+    const [game, setGame] = useState(new Game(difficulties[selectedDifficulty],chosenAlphabet))
 
     //#TODO: Add more interactivity, maybe spawn new words randomly, or also provide the world translation
     //needs polishing overall
@@ -46,13 +59,17 @@ const FallingWords = () => {
         setIsStarted(true)
         setScore(0)
     }
+
     useEffect(() => {
         window.addEventListener('resize',loadGame)
         return () => window.removeEventListener('resize',loadGame)
     },[])
-    function loadGame(difficultyId) {
+
+    function loadGame(difficultyId,alphabet) {
         difficultyId = Number.isFinite(difficultyId) ? difficultyId : selectedDifficulty 
-        let game = new Game(difficulties[difficultyId])
+        alphabet = alphabet ? alphabet : chosenAlphabet
+        console.log(alphabet)
+        let game = new Game(difficulties[difficultyId],alphabet)
         let words = [game.getRandomWord()]
         setGame(game)
         setWords(words)
@@ -73,12 +90,16 @@ const FallingWords = () => {
         })
         setWords(w)
     }
-
+    function handleAlphabetChange(alphabet) {
+        setChosenAlphabet(alphabet)
+        loadGame(selectedDifficulty,alphabet)
+    }
     function handleInput(e) {
         let value = e.target.value
         setCurrentText(value)
         if (!value.endsWith(" ") || words.length === 0) return
-        let correctWord = words.findIndex(word => word.text === value.trim())
+        value = value.trim()
+        let correctWord = words.findIndex(word => word.text === value || word.romanization === value)
         if (correctWord > -1) {
             setCurrentText('')
             changeInputColor('#10b981')
@@ -96,11 +117,19 @@ const FallingWords = () => {
         <div className="flex flex-col items-center space-y-2">
             <div className='flex flex-wrap gap-1 p-2'>
                 {difficulties.map(difficulty => <button
+                    id={difficulty.id}
                     className={`${difficulty.id === selectedDifficulty ? "bg-primary" : "bg-gray-200 dark:bg-gray-700"} p-2 px-4 dark:hover:text-white bg-opacity-80 hover:bg-opacity-70 dark:hover:bg-opacity-70 rounded-lg cursor-pointer`}
                     onClick={() => handleDifficultyChange(difficulty.id)}
                 >
                     {difficulty.name}
                 </button>)}
+                <select 
+                    className="bg-gray-200 dark:bg-gray-700 p-2 px-4 dark:hover:text-white bg-opacity-80 hover:bg-opacity-70 dark:hover:bg-opacity-70 rounded-lg cursor-pointer"
+                    onChange={e => handleAlphabetChange(e.target.value.toLowerCase())}
+                >
+                    <option>Hiragana</option>
+                    <option>Katakana</option>
+                </select>
             </div>
 
             <div className='flex-col space-y-2 '>
@@ -160,7 +189,7 @@ const FallingWords = () => {
 }
 
 class Game {
-    constructor(difficulty) {
+    constructor(difficulty,alphabet) {
         this.width = window.innerWidth / 2
         this.height = window.innerWidth / 4
         if (window.innerHeight > window.innerWidth) {
@@ -169,7 +198,7 @@ class Game {
         }
         this.tick = 0.2
         this.speed = difficulty.speed
-        this.words = difficulty.words.map(word => { return new Word(word, this.width) })
+        this.words = difficulty.words[alphabet].map(word => { return new Word(word, this.width) })
     }
     getRandomWord() {
         let word = this.words[Math.floor(Math.random() * this.words.length)];
@@ -181,6 +210,8 @@ class Game {
 class Word {
     constructor(wordData, width) {
         this.text = wordData.expression;
+        this.romanization = toRomaji(wordData.expression);
+        this.meaning = wordData.meaning;
         this.x = (Math.random() * width / 2) - width / 4 + width / 3;
         this.y = 0
     }
